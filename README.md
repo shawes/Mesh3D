353e174b3e2f9f364ec59536234a5e280021d1c7
stevehawes <maxhawes@gmail.com>
2015-11-19 09:17:44 +1100

    Fixed up the usages and added to the README.md

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Mesh3D
 
-This is the first commit of the readme
+Mesh3D is a command line tool that splits up rectangular 3D meshes into a number of specified qaudrats and calculates the area of each quadrat.
+
+The command line usage for this application is specified as:
+```
+
+Usage: mesh3d [OPTIONS] outputfile [input] ...
+
+OPTIONS
+
+--dim n     Calculate the 3D (3) or 2D (2) area of the faces
+
+--length n  The number to subdivide the rectangular mesh's length.
+
+--width n   The number to subdivide the rectangular mesh's width.
+
+PARAMETERS
+
+outputfile  Output file to which to write (a .csv)
+
+input       Input .x3d files to read. If not specified, use stdin. (May be
+            specified multiple times.)
+```
+
+The general gist of the algorithm are:
+1. Read in a list of X3D files that specify retangular meshes.
+2. Find the maximum bounding retangule that fits inside all the meshes
+3. Subdivide the each mesh into the specified number of quadrats using coordinate geometry
+4. Calculate the area face where the centroid fits into of each quadrat, using the dimensions specified
+5. Print the output to a .csv file
+
--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -7,12 +7,12 @@ object Driver {
   import ArgotConverters._
 
   // Setup the command line arguments
-  val parser = new ArgotParser("mesh3d", preUsage = Some("Version 1.0"))
-  val width = parser.option[Int](List("width"), "width ratio", "the ratio of the rectangles width")
-  val length = parser.option[Int](List("l", "length"), "length ratio", "the ratio of the rectangles length")
-  val dim = parser.option[Int](List("d", "dimension"), "dimension", "the ratio of the rectangles length")
-  val output = parser.parameter[String]("outputfile", "Output file to which to write.", optional = false)
-  val input = parser.multiParameter[File]("input", "Input files to read. If not specified, use stdin.", optional = true) {
+  val parser = new ArgotParser("mesh3d", preUsage = Some("Mesh3D, Author: Steven Hawes, Version 1.0"))
+  val width = parser.option[Int](List("width"), "n", "The number to subdivide the rectangular mesh's width.")
+  val length = parser.option[Int](List("length"), "n", "The number to subdivide the rectangular mesh's length.")
+  val dim = parser.option[Int](List("dim"), "n", "Calculate the 3D (3) or 2D (2) area of the faces")
+  val output = parser.parameter[String]("outputfile", "Output file to which to write (a .csv)", optional = false)
+  val input = parser.multiParameter[File]("input", "Input .x3d files to read. If not specified, use stdin.", optional = true) {
     (s, opt) =>
       val file = new File(s)
       if (!file.exists)
@@ -40,11 +40,9 @@ object Driver {
     val passes = files.map(x => reader.read(x))
 
     val is3DArea = dim.value.getOrElse(0) == 3
-    val meshs = passes.map(x => new Mesh(x, is3DArea))
+    val meshes = passes.map(x => new Mesh(x, is3DArea))
 
-
-
-    val rectangle = geometry.findMaximumBoundingBox(meshs)
+    val rectangle = geometry.findMaximumBoundingBox(meshes)
 
     println("Width distance: " + rectangle.a.distanceXY(rectangle.b) + " and " + rectangle.d.distanceXY(rectangle.c))
     println("Length distance: " + rectangle.a.distanceXY(rectangle.d) + " and " + rectangle.b.distanceXY(rectangle.c))
@@ -54,7 +52,7 @@ object Driver {
 
     val polygons = rectangleSubDivider.divideRectangle(rectangle)
 
-    val areas = meshs.map(x => x.getAreas(polygons))
+    val areas = meshes.map(x => x.getAreas(polygons))
 
     //val quadrats = range(List(0, 0), List(width.value.getOrElse(0), length.value.getOrElse(0)))
     //println(quadrats)
